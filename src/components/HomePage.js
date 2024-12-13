efeebe8fa01a619c3922e9faff7d742dde541db3
marguptl <margilpatel57@gmail.com>
2024-12-13 14:13:41 -0500

    Reset staging to remove large files

--- src/components/HomePage.js
+++
@@ -1,8 +0,0 @@
-// HomePage.js
-import React from 'react';
-
-const HomePage = () => {
-  return <h1>Welcome to the PetsCrew Home Page</h1>;
-};
-
-export default HomePage;
--- src/components/UserProfile.js
+++
@@ -1,8 +0,0 @@
-// UserProfile.js
-import React from 'react';
-
-const UserProfile = () => {
-  return <h1>User Profile Page</h1>;
-};
-
-export default UserProfile;
